7e49abb1c2317bd662796443015e140a4d8d2879
Tobe O <thosakwe@gmail.com>
2018-11-07 00:38:13 -0500

    connection.dart

---
+++ lib/src/connection.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+var _connections = 0;
+
+class Connection<T> {
+  static final _rnd = new Random();
+
+  // TODO: What is the type of these...?
+  final T from, to;
+
+  final ID = uid();
+
+  double weight;
+
+  var gain = 1;
+
+  // TODO: gater?
+  var gater;
+
+  Connection(this.from, this.to, [this.weight]) {
+    assert(from != null && to != null, 'Connection Error: Invalid neurons');
+    weight ??= _rnd.nextDouble() * .2 - .1;
+  }
+
+  static int uid() => _connections++;
+}
